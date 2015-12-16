00165fe49f2d642ea23de354bd3ab7709475e4fc
sujjjith <sujjjith@gmail.com>
2015-12-16 14:27:57 +0530

    adding special stack with getmin and getmax

--- src/main/java/ds/stack/SpecialStack.java
+++ src/main/java/ds/stack/SpecialStack.java
@@ -3,5 +3,58 @@ package ds.stack;
 /**
  * Created by skupunarapu on 12/15/2015.
  */
-public class SpecialStack {
+public class SpecialStack extends Stack {
+    Stack minStack = new Stack();
+    Stack maxStack = new Stack();
+
+    public void push(int ele){
+        if(isEmpty()){
+            super.push(ele);
+            minStack.push(ele);
+            maxStack.push(ele);
+        }else{
+            if(minStack.stack[TOP] > ele){
+                super.push(ele);
+                minStack.push(ele);
+                maxStack.push(maxStack.stack[TOP - 1]);
+            }else if(maxStack.stack[TOP] < ele){
+                super.push(ele);
+                maxStack.push(ele);
+                minStack.push(minStack.stack[TOP - 1]);
+            }
+        }
+    }
+
+    public int pop(){
+        minStack.pop();
+        maxStack.pop();
+        return super.pop();
+    }
+
+    public int getMin(){
+        int min = minStack.pop();
+        minStack.push(min);
+        return min;
+    }
+
+    public int getMax(){
+        int max = maxStack.pop();
+        maxStack.push(max);
+        return max;
+    }
+
+    public static void main(String[] args) {
+        SpecialStack s = new SpecialStack();
+        s.push(10);
+        s.push(2);
+        s.push(20);
+        s.push(30);
+        s.push(15);
+        System.out.println("Max "+s.getMax());
+        System.out.println("Min "+s.getMin());
+        s.push(5);
+        s.push(50);
+        System.out.println("Max "+s.getMax());
+        System.out.println("Min "+s.getMin());
+    }
 }
--- src/main/java/ds/stack/Stack.java
+++ src/main/java/ds/stack/Stack.java
@@ -4,14 +4,9 @@ package ds.stack;
  * Created by skupunarapu on 12/15/2015.
  */
 public class Stack {
-    private int TOP = -1;
-    private static final int MAX = 10;
-    private int[] stack = new int[MAX];
-    private int[] minStack = new int[MAX];
-    private int[] maxStack = new int[MAX];
-
-    private int MIN_STACK = Integer.MAX_VALUE;
-    private int MAX_STACK = Integer.MIN_VALUE;
+    int TOP = -1;
+    static final int MAX = 10;
+    int[] stack = new int[MAX];
 
     public boolean isEmpty(){
         if(TOP == -1)
@@ -27,7 +22,7 @@ public class Stack {
             return false;
     }
 
-    public int pop(int[] stack) {
+    public int pop() {
         int popEle = 0;
         if (!isEmpty()) {
             popEle = stack[TOP];
@@ -36,14 +31,10 @@ public class Stack {
         return popEle;
     }
 
-    public void push(int[] stack, int ele){
+    public void push(int ele){
         if(!isFull()){
             TOP = TOP + 1;
             stack[TOP] = ele;
-            if(ele < MIN_STACK)
-                MIN_STACK = ele;
-            else if(ele > MAX_STACK)
-                MAX_STACK = ele;
         }
     }
 }
